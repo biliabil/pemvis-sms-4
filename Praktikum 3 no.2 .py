40a0b9edb3d82764090289388ef2c2596d426589
biliabil <90602209+biliabil@users.noreply.github.com>
2022-03-30 21:31:45 +0700

    Create Praktikum 3 no.2 .py

---
+++ Praktikum 3 no.2 .py
@@ -0,0 +1,10 @@
+# sitti aulia sabina r
+# 20051397016 
+# 2020B 
+
+desimal = int(input('Masukkan Bilangan Desimal = '))
+
+biner = bin(desimal) .replace("0b","")
+oktal = oct(desimal) .replace("0o","")
+hexa = hex(desimal) .replace("0x","")
+print(biner,oktal,hexa)
